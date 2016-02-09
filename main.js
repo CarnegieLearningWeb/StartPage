68dc56b67605547f9c7ff80e09f31688d81f900c
joan <joancc@gmail.com>
2016-02-09 00:13:20 -0500

    Basic setup for datampas, vue-webpack

---
+++ main.js
@@ -0,0 +1,16 @@
+// main.js
+var Vue = require('vue')
+// require a *.vue component
+var App = require('./src/components/App.vue')
+var Grid = require('./src/components/Grid.vue')
+
+// mount a root Vue instance
+new Vue({
+  el: 'body',
+  components: {
+    // include the required component
+    // in the options
+    app: App,
+    grid: Grid
+  }
+})
---
+++ src/components/App.vue
@@ -0,0 +1,34 @@
+<style scoped>
+
+</style>
+
+<template>
+  <grid 
+    :data="[{ name: 'RTC', link: 'rtc' },
+            { name: 'AAA', link: 'aaa' },
+            { name: 'TWYLS', link: 'tywls' }]"
+    :columns="['name', 'link']"
+    >
+    </grid>
+
+</template>
+
+<script>
+    import Grid from './Grid.vue';
+
+
+    export default{
+        props: {
+            
+        },
+        data() {
+            
+        },
+        methods: {
+            
+        },
+        components:{
+          grid: Grid
+        }
+    }
+</script>
